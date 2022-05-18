ba7da261fffdbe56a9b2ded69bf65ab21f3eab71
MirrorBot111 <103623721+MirrorBot111@users.noreply.github.com>
2022-05-18 21:33:23 +0530

    Update gdriveTools.py

--- bot/helper/drive_utils/gdriveTools.py
+++ bot/helper/drive_utils/gdriveTools.py
@@ -10,7 +10,7 @@ from urllib.parse import parse_qs
 from random import randrange
 from timeit import default_timer as timer
 
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
 from telegraph.exceptions import RetryAfterError
 
 from google.auth.transport.requests import Request
@@ -243,10 +243,7 @@ class GoogleDriveHelper:
                 msg += f"\n<b>Type: </b>Folder"
                 msg += f"\n<b>SubFolders: </b>{self.total_folders}"
                 msg += f"\n<b>Files: </b>{self.total_files}"
-                    reply_markup = InlineKeyboardMarkup( [[
-                        InlineKeyboardButton("DRIVE LINK", url="https://t.me/mhd_thanzeer")
-                        ]]
-                        )
+                msg += f'\n\n<a href="{self.__G_DRIVE_DIR_BASE_DOWNLOAD_URL.format(dir_id)}">Drive Link</a>'
                 if DRIVE_INDEX_URL is not None:
                     url = requests.utils.requote_uri(f'{DRIVE_INDEX_URL}/{meta.get("name")}/')
                     msg += f' | <a href="{url}">Index Link</a>'
@@ -257,10 +254,8 @@ class GoogleDriveHelper:
                     mime_type = 'File'
                 msg += f'\n<b>Size: </b>{get_readable_file_size(int(meta.get("size", 0)))}'
                 msg += f'\n<b>Type: </b>{mime_type}'
-                    reply_markup = InlineKeyboardMarkup( [[
-                        InlineKeyboardButton("DRIVE LINK", url="https://t.me/mhd_thanzeer")
-                        ]]
-                        )
+                surl = self.__G_DRIVE_BASE_DOWNLOAD_URL.format(file.get("id"))}
+                buttons.buildbutton("☁️ Dʀɪᴠᴇ Lɪɴᴋ ☁️", surl)
                 if DRIVE_INDEX_URL is not None:
                     url = requests.utils.requote_uri(f'{DRIVE_INDEX_URL}/{file.get("name")}')
                     msg += f' | <a href="{url}">Index Link</a>'
